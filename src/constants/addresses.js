842362067da9cc618f4ac13e451ebaa1220a9eec
eben619 <ebenyawboateng72@gmail.com>
2025-01-15 15:02:27 +0000

    update

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -1,4 +1,4 @@
 export const crowdfundContractAddress =
-    "0x85a0599fa1c6231e632582dd4aec2f48a59b19ac";
+    "0xd468af44bcb640055226149d3f9d4ff1c5642036";
 export const multcall2ContractAddress =
     "0x7a37FcaBaCC6b04422f2A625080cEf2D1B4B6E4e";
